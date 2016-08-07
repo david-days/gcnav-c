f15704205950edcd091ea5e7ec4a8fd81c9fe69c
David Days <david.c.days@gmail.com>
2016-08-07 03:35:21 -0400

    Small refactor of CMakeLists.txt test project added

---
+++ tests/include/gctest.h
@@ -0,0 +1,8 @@
+//
+// Created by david on 8/7/16.
+//
+
+#ifndef GCNAV_GCTEST_H
+#define GCNAV_GCTEST_H
+
+#endif //GCNAV_GCTEST_H
---
+++ tests/src/testrunner.c
@@ -0,0 +1,9 @@
+//
+// Created by david on 8/7/16.
+//
+#include <gctest.h>
+
+int main (int argc, char *argv[])
+{
+
+}
