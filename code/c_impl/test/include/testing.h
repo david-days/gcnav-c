ad695f141ed403cd6bd9e13601b40d547d2cb04f
david.c.days@gmail.com <david.c.days@gmail.com>
2016-10-14 13:11:03 -0400

    Reworking project structure for testing

---
+++ code/c_impl/test/include/testing.h
@@ -0,0 +1,16 @@
+/**
+ * Testing header file
+ *
+**/
+
+#ifndef GCNAV_C_TESTING
+#define GCNAV_C_TESTING
+
+// Minunit testing framework--switch out (or move) if using CUnit or others
+#include <minunit.h>
+
+// Tested code headers
+#include <utils/mathutils.h>
+
+
+#endif //GCNAV_C_TESTING 
